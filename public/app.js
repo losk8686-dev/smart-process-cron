55c481ad2b930ae0de45ff56f19c63aef7c3bd60
Алексей Лоскутов <a.l@Mac-mini-Aleks.local>
2026-06-25 12:08:07 +0700

    Fix React 18 render API

--- public/app.js
+++ public/app.js
@@ -481,4 +481,5 @@ function TaskModal({ stages, businessProcesses, onClose, onSave }) {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
